6a873fdf53b8a95afe13ca1022ffb715614ffee0
mcnamacl <mcnamacl@tcd.ie>
2019-11-07 14:58:04 +0000

    Prints out my followers to the console

--- app.py
+++ app.py
@@ -1,7 +1,17 @@
 from github import Github
+from flask import Flask, request, render_template, jsonify
+import sys
+
+app = Flask(__name__)
 
 g = Github("mcnamacl", "password")
+userName = g.get_user().name
 
-name = g.get_user().name
-print(name)
+@app.route("/")
+def index():
+    return render_template("index.html")
 
+def getFollowers():
+    index = 0
+    for follower in g.get_user().get_followers():
+        print(follower, file=sys.stderr)
