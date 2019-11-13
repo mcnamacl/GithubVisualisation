8716c8f0ba0b0c4f74aeaceec9567ed30fc99758
mcnamacl <mcnamacl@tcd.ie>
2019-11-13 22:24:29 +0000

    Began the correlation between number of repos and number of followers function.

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from github import Github
 from flask import Flask, request, render_template, jsonify
+from collections import defaultdict
 import sys
+import json
 
 app = Flask(__name__)
 
@@ -12,6 +14,13 @@ def index():
     repos, numOfRepos = getRepos(g.get_user())
     followers, numOfFollowers = getFollowers(g.get_user())
     return render_template("index.html", repos=repos)
+
+def corrFollowersRepos(user):
+    repos, numOfRepos = getRepos(user)
+    followers, numOfFollowers = getFollowers(user)
+    for follower in followers:
+        _, numFollower = getFollowers(followers[follower])
+        _, numRepo = getRepos(followers[follower])
 
 def getRepos(user):
     repos = {}
