5012396b46d817c8e4cc11c4cf83623cc337d855
mcnamacl <mcnamacl@tcd.ie>
2019-11-15 20:35:33 +0000

    Fixed a number of functions and added one that will get the top 1000 repos.

--- app.py
+++ app.py
@@ -11,20 +11,22 @@ userName = g.get_user().name
 
 @app.route("/")
 def index():
-    repos, numOfRepos = getRepos(g.get_user())
-    followers, numOfFollowers = getFollowers(g.get_user())
-    corrFollwersRepos = corrFollowersRepos(g.get_user())
+    repos = getRepos(g.get_user())
+    corrFollowersRepos(g.get_user())
+    topRepos = getTopRepos()
+    print(topRepos.totalCount, file=sys.stderr)
     return render_template("index.html", repos=repos)
 
 def corrFollowersRepos(user):
-    repos, numOfRepos = getRepos(user)
-    followers, numOfFollowers = getFollowers(user)
+    followers = getFollowers(user)
     result = {}
-    for follower in followers:
-        _, numFollower = getFollowers(followers[follower])
-        _, numRepo = getRepos(followers[follower])
+    for followerIt in followers:
+        follower = followers[followerIt]
+        result[str(follower)] = {}
+        numFollower = follower.get_followers().totalCount
+        numRepo = follower.get_repos().totalCount
         tmp = {numFollower : numRepo}
-        result.update(tmp)
+        result[str(follower)] = tmp
     print(json.dumps(result), file=sys.stderr)
 
 def getRepos(user):
@@ -33,7 +35,10 @@ def getRepos(user):
     for repo in user.get_repos():
         repos[str(index)] = repo.name
         index = index + 1
-    return repos, index
+    return repos
+
+def getTopRepos():
+    return g.search_repositories(query='stars:>=500', sort='stars', order='desc')
 
 def getFollowers(user):
     followers = {}
@@ -41,4 +46,4 @@ def getFollowers(user):
     for follower in user.get_followers():
         followers[str(index)] = follower
         index = index + 1
-    return followers, index
+    return followers
