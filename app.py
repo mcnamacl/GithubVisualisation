85841df112605bcb233ac42158db3e4092380f55
mcnamacl <mcnamacl@tcd.ie>
2019-11-19 22:08:29 +0000

    Created function to read in json files.

--- app.py
+++ app.py
@@ -10,3 +10,12 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     return render_template("index.html")
+
+def createFlowMatrix():
+    repoLanguages = {}
+    with open('repoLanguages.txt') as json_file:
+        repoLanguages = json.load(json_file)
+
+    userLanguages = {}
+    with open('userLanguages.txt') as json_file:
+        userLanguages = json.load(json_file)
