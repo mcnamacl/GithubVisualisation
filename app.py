86a51b028d244b100d383d1ae1ae8d3184d54750
claire mcnamara <mcnamacl@tcd.ie>
2019-12-08 19:39:46 +0000

    Made the number of correlations seen in the chord chart dynamic.

--- app.py
+++ app.py
@@ -10,11 +10,17 @@ import random
 app = Flask(__name__)
 
 
-@app.route("/")
+@app.route("/", methods=["POST", "GET"])
 def index():
-    matrix, xAxis, yAxis = createFlowMatrix()
+    num = 10
+    newNum = request.form.get("numOfCorr")
+    if newNum is not None:
+        newNum = int(newNum)
+        if isNumber(newNum) and newNum > 0 and newNum < 30:
+            num = newNum
+    matrix, xAxis, yAxis = createFlowMatrix(num)
     labels = xAxis + yAxis
-    complang = createLabelsColoursCSV(labels)
+    complang = createLabelsColoursCSV(labels, num)
     return render_template("index.html", matrix=matrix, complang=complang)
 
 
@@ -38,20 +44,26 @@ def picecharts():
             title = "Combined Top Repository/Top User Breakdown by Language"
     return render_template("piecharts.html", piechart=piechart, title=title)
 
+def isNumber(s):
+    try:
+        int(s)
+        return True
+    except ValueError:
+        return False
 
-def createFlowMatrix():
+def createFlowMatrix(finalNum):
     compLangArr = readInFile("corrCompanyLanguages.txt")
-    topTenLanguages = getTopTenLanguages(compLangArr)
-    topTenCompanies = getTopTenCompanies(compLangArr)
+    topXLanguages = getTopXNumLanguages(compLangArr, finalNum)
+    topXCompanies = getTopXNumCompanies(compLangArr, finalNum)
     jsonMatrix = {}
     jsonCompanies = []
     jsonLanguages = []
     rowIndex = 1
     gotLang = False
-    for comp in topTenCompanies:
+    for comp in topXCompanies:
         jsonMatrix[str(rowIndex)] = []
         rowList = []
-        for lang in topTenLanguages:
+        for lang in topXLanguages:
             if lang in compLangArr[comp]:
                 num = compLangArr[comp][lang]
                 rowList.append(num)
@@ -64,12 +76,12 @@ def createFlowMatrix():
         jsonCompanies.append(comp)
         gotLang = True
 
-    finalMatrix = createFinalMatrix(jsonMatrix)
+    finalMatrix = createFinalMatrix(jsonMatrix, finalNum)
     return finalMatrix, jsonCompanies, jsonLanguages
 
 
-# Gets top ten language by use per company
-def getTopTenLanguages(compLangArr):
+# Gets top X num of languages by use per company
+def getTopXNumLanguages(compLangArr, finalNum):
     topLanguages = {}
     for comp in compLangArr:
         for lang in compLangArr[comp]:
@@ -82,57 +94,58 @@ def getTopTenLanguages(compLangArr):
     sortedLanguages = OrderedDict(
         sorted(topLanguages.items(), key=lambda x: x[1], reverse=True)
     )
-    topTenLanguages = []
+    topXLanguages = []
     index = 0
     for lang in sortedLanguages:
-        topTenLanguages.append(lang)
+        topXLanguages.append(lang)
         index = index + 1
-        if index == 10:
+        if index == finalNum:
             break
-    return topTenLanguages
+    return topXLanguages
 
 
-# Gets top ten companies by variety
-def getTopTenCompanies(compLangArr):
+# Gets top X num of companies by variety
+def getTopXNumCompanies(compLangArr, finalNum):
     companies = {}
     for comp in compLangArr:
         companies[comp] = len(compLangArr[comp])
     sortedLanguages = OrderedDict(
         sorted(companies.items(), key=lambda x: x[1], reverse=True)
     )
-    topTenCompanies = []
+    topXCompanies = []
     index = 0
     for lang in sortedLanguages:
-        topTenCompanies.append(lang)
+        topXCompanies.append(lang)
         index = index + 1
-        if index == 10:
+        if index == finalNum:
             break
-    return topTenCompanies
+    return topXCompanies
 
 
-def createFinalMatrix(matrix):
-    finalMatrix = [[0] * 20 for i in range(20)]
+def createFinalMatrix(matrix, finalNum):
+    rangeNum = finalNum * 2
+    finalMatrix = [[0] * rangeNum for i in range(rangeNum)]
     index = 0
     for x in matrix:
-        j = 10
-        while j < 20:
-            finalMatrix[index][j] = matrix[x][0][j - 10]
+        j = finalNum
+        while j < rangeNum:
+            finalMatrix[index][j] = matrix[x][0][j - finalNum]
             j = j + 1
         index = index + 1
 
     index = 0
     for x in matrix:
-        j = 10
-        while j < 20:
-            finalMatrix[j][index] = matrix[x][0][j - 10]
+        j = finalNum
+        while j < rangeNum:
+            finalMatrix[j][index] = matrix[x][0][j - finalNum]
             j = j + 1
         index = index + 1
         
     return finalMatrix
 
 
-def createLabelsColoursCSV(labels):
-    finalLabels = [[0] * 2 for i in range(21)]
+def createLabelsColoursCSV(labels, finalNum):
+    finalLabels = [[0] * 2 for i in range(finalNum * 2)]
     index = 0
     for label in labels:
         finalLabels[index][0] = label
